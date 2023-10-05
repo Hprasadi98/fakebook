95f30d614077c26a226e66c33cfa2e1adeb1fa2d
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2023-10-05 20:12:39 +0530

    left side icon bar
    create left side icon bar basically

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import Navbar from '../../components/Navigation/Navbar'
+import LeftPane from '../../components/LeftPane/LeftPane'
+import PostPane from '../../components/PostPane/PostPane'
+import RightPane from '../../components/RightPane/RightPane'
+import './home.css'
 
 export default function Home() {
   return (
+    <>
     <Navbar/>
+    <div className="bottomContainer">
+      <LeftPane/>
+      <PostPane/>
+      <RightPane/>
+    </div>
+    </>
   )
 }
