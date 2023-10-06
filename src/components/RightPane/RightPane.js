38e16217a54244a82831de0351902d41bf9012e2
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2023-10-06 20:35:41 +0530

    rightpane complete
    create add session online status friends session create and styling

--- src/components/RightPane/RightPane.js
+++ src/components/RightPane/RightPane.js
@@ -3,6 +3,57 @@ import './rightPane.css'
 
 export default function RightPane() {
   return (
-    <div className='rightPaneBox'>RightPane</div>
+    <div className='rightPaneBox'>
+      <div className="rightPaneContainer">
+        <div className="adContainer">
+          <span className="sponserd">Sponsored</span>
+          <img src="/images/add.png" alt="" className="adImage" />
+          <span className="adText">
+            Publish your Adds by using mour fb page of HasiAdds. We can publish your adds definetly. Like our page now..!
+          </span>
+        </div>
+        <div className="birthdayContainer">
+          <img src="/images/bd.png" alt="" className="birthdayImage" />
+          <span className="birthdayText"><b>Sadali Malshika</b> and <b>3 others</b> having birthday today</span>
+        </div>
+          <div className="friendList">
+            <li className="onlineFriend">
+              <div className="onlineFriendImageContainer">
+                <img src="/images/bgirl.jpeg" alt="" className="onlineFriendImg" />
+                <span className="onlineStatus"></span>
+              </div>
+              <span className="onlineFriendName">Rukshani Pathirana</span>
+            </li>
+            <li className="onlineFriend">
+              <div className="onlineFriendImageContainer">
+                <img src="/images/bgirl.jpeg" alt="" className="onlineFriendImg" />
+                <span className="onlineStatus"></span>
+              </div>
+              <span className="onlineFriendName">Rukshani Pathirana</span>
+            </li>
+            <li className="onlineFriend">
+              <div className="onlineFriendImageContainer">
+                <img src="/images/bgirl.jpeg" alt="" className="onlineFriendImg" />
+                <span className="onlineStatus"></span>
+              </div>
+              <span className="onlineFriendName">Rukshani Pathirana</span>
+            </li>
+            <li className="onlineFriend">
+              <div className="onlineFriendImageContainer">
+                <img src="/images/bgirl.jpeg" alt="" className="onlineFriendImg" />
+                <span className="onlineStatus"></span>
+              </div>
+              <span className="onlineFriendName">Rukshani Pathirana</span>
+            </li>
+            <li className="onlineFriend">
+              <div className="onlineFriendImageContainer">
+                <img src="/images/bgirl.jpeg" alt="" className="onlineFriendImg" />
+                <span className="onlineStatus"></span>
+              </div>
+              <span className="onlineFriendName">Rukshani Pathirana</span>
+            </li>
+          </div>
+      </div>
+    </div>
   )
 }
